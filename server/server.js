e66847f1e38cbe7ba8d3a42a251387e93771485a
Hurry-sh <141551702+Hurry-sh@users.noreply.github.com>
2025-04-18 18:28:17 +0530

    Updated project files

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get("/recipeStream", async (req, res) => {
 });
 
 async function fetchGeminiCompletionsStream(prompt, callback) {
-  const GEMINI_API_KEY = "AIzaSyCdfp1-Lz1DnL0InSodwdMRqxZoOUOIqPo"; // Replace with actual API key
+  const GEMINI_API_KEY = ""; // Replace with actual API key
   const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
 
